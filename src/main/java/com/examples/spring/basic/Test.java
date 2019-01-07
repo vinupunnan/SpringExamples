c0ea33f89a553cbef90aa7708f6a244ee272a3f5
Vinod Pillai <vinupunnan@gmail.com>
2019-01-07 16:05:01 -0500

    changes in messages

--- src/main/java/com/examples/spring/basic/Test.java
+++ src/main/java/com/examples/spring/basic/Test.java
@@ -4,21 +4,23 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Test {
 	public static void main(String args[]) {
-		FileSystemXmlApplicationContext context =	new FileSystemXmlApplicationContext("beans.xml");
+		FileSystemXmlApplicationContext context =	new FileSystemXmlApplicationContext("Integrationex.xml");
+		while (true) {
+		}
 		
 //		HelloWorld hello =(HelloWorld) context.getBean("helloWorld");
-//		hello.test();
+//	hello.test();
 //		
 //		Person p = (Person) context.getBean("person");
 //		System.out.println(p.toString());
-
-		User user = (User) context.getBean("user");
-		System.out.println("Car Detaugls is as follows" +user.getCar().toString());
+//
+//		User user = (User) context.getBean("user");
+//		System.out.println("Car Detaugls is as follows" +user.getCar().toString());
 		
 		
 		
 		
-		context.close();
+		//context.close();
 	}
 
 	
